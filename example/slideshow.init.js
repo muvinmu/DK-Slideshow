7a098dae968020af839c748c57d6b02b779e9b3e
muvinmu <davidkolarjh@gmail.com>
2016-10-24 21:42:44 +0200

    add example and src

---
+++ example/slideshow.init.js
@@ -0,0 +1,20 @@
+// První slideshow
+$(".slideshow--top").slideshow({
+    "autoPlaySpeed" : 2500,
+    "fadeSpeed"     : 500,
+    "hoverStop"     : true,
+    "nav"           : true,
+    "dots"          : true
+});
+
+// Druhá slideshow
+$(".slideshow--left").slideshow({
+    "hoverStop" : true
+});
+
+// Třetí slideshow
+$(".slideshow--right").slideshow({
+    "hoverStop" : true,
+    "nav"       : true,
+    "dots"      : true
+});
